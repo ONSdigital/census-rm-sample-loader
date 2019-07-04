01d5ea22d195a27e3681e62d8374f681d5cceb7c
Richard Weeks <richard.weeks@qa.com>
2019-07-04 09:56:33 +0100

    Changes to work from command line.

--- load_sample.py
+++ load_sample.py
@@ -12,13 +12,12 @@ def parse_arguments():
     parser.add_argument('sample_file_path', help='path to the sample file', type=str)
     parser.add_argument('collection_exercise_id', help='collection exercise ID', type=str)
     parser.add_argument('action_plan_id', help='action plan ID', type=str)
-    parser.add_argument('collection_instrument_id', help='collection instrument ID', type=str)
     return parser.parse_args()
 
 
-def load_sample_file(sample_file_path, collection_exercise_id, action_plan_id, collection_instrument_id):
+def load_sample_file(sample_file_path, collection_exercise_id, action_plan_id):
     with open(sample_file_path) as sample_file:
-        load_sample(sample_file, collection_exercise_id, action_plan_id, collection_instrument_id)
+        load_sample(sample_file, collection_exercise_id, action_plan_id)
 
 
 def load_sample(sample_file: Iterable[str], collection_exercise_id: str, action_plan_id: str):
@@ -69,8 +68,7 @@ def _create_case_json(sample_row, collection_exercise_id, action_plan_id) -> str
 
 def main():
     args = parse_arguments()
-    load_sample_file(args.sample_file_path, args.collection_exercise_id, args.action_plan_id,
-                     args.collection_instrument_id)
+    load_sample_file(args.sample_file_path, args.collection_exercise_id, args.action_plan_id)
 
 
 if __name__ == "__main__":
