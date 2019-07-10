f2d6df71ba75a84d7fa2fcf51f8ee8f1d7920541
Richard Weeks <richard.weeks@qa.com>
2019-07-10 12:43:24 +0100

    Fix Pep8.

--- validate_sample.py
+++ validate_sample.py
@@ -45,6 +45,7 @@ def load_sample(sample_file):
         validate_mmtopo_toid(count, sample_row)
         validate_ce_expected_capacity(count, sample_row)
 
+
 def validate_arid(count, sample_row):
     column = 'ARID'
     maximum_length = 21
