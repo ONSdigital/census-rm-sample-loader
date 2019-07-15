a01c1275b6051b4aea522523b28d811d0b192f6a
Luke Loze <Luke.Loze@ext.ons.gov.uk>
2019-07-15 15:31:40 +0100

    run in k8s config

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM python:3.6-slim
 ENV RABBITMQ_HOST rabbitmq
 ENV RABBITMQ_PORT 5672
 ENV RABBITMQ_VHOST /
-ENV RABBITMQ_QUEUE Case.CaseDelivery.binding
+ENV RABBITMQ_QUEUE case.sample.inbound
 ENV RABBITMQ_USER guest
 ENV RABBITMQ_PASSWORD guest
 ENV RABBITMQ_EXCHANGE collection-outbound-exchange
--- run_in_kubernetes.sh
+++ run_in_kubernetes.sh
@@ -2,4 +2,11 @@
 kubectl run sampleloader -it --rm \
     --generator=run-pod/v1 \
     --image eu.gcr.io/census-rm-ci/census-rm-sample-loader \
+    --env=RABBITMQ_USER=$(kubectl get secret rabbitmq -o=jsonpath="{.data.rabbitmq-username}" | base64 --decode) \
+    --env=RABBITMQ_PASSWORD=$(kubectl get secret rabbitmq -o=jsonpath="{.data.rabbitmq-password}" | base64 --decode) \
+    --env=RABBIT_SERVICE_HOST=rabbitmq \
+    --env=RABBITMQ_SERVICE_PORT=5572 \
+    --env=RABBITMQ_QUEUE='case.sample.inbound' \
+    --env=RABBITMQ_VHOST='/' \
+    --env=RABBITMQ_EXCHANGE='' \
     -- /bin/bash
