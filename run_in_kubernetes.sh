d51a881611809a302277b1dec52512fd8f13482d
Luke Loze <Luke.Loze@ext.ons.gov.uk>
2019-07-16 09:15:08 +0100

    Update run in k8s with corrected image location

--- run_in_kubernetes.sh
+++ run_in_kubernetes.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 kubectl run sampleloader -it --rm \
     --generator=run-pod/v1 \
-    --image eu.gcr.io/census-rm-ci/census-rm-sample-loader \
+    --image eu.gcr.io/census-rm-ci/rm/census-rm-sample-loader \
     --env=RABBITMQ_USER=$(kubectl get secret rabbitmq -o=jsonpath="{.data.rabbitmq-username}" | base64 --decode) \
     --env=RABBITMQ_PASSWORD=$(kubectl get secret rabbitmq -o=jsonpath="{.data.rabbitmq-password}" | base64 --decode) \
     --env=RABBIT_SERVICE_HOST=rabbitmq \
