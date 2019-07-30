e06bcb50ba36372bc30eb28b7e95141679153bce
AdamHawtin <adam.hawtin@ons.gov.uk>
2019-07-30 15:04:09 +0100

    Experimental rewrite of validation

--- validate_sample.py
+++ validate_sample.py
@@ -1,10 +1,19 @@
 import argparse
 import csv
+from collections import namedtuple
+
+from validators import MaxLength, Unique, ValidationError, Mandatory, IsNumeric, InSet, SetEqual, IsFloat, \
+    MaxDecimalScale, MaxDecimalPrecision
 
 ARID = set()
 
 
-VALID_ESTABLISHMENT_TYPES = {
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Load a sample file into response management.')
+    parser.add_argument('sample_file_path', help='path to the sample file', type=str)
+    return parser.parse_args()
+
+ESTAB_TYPES = {
     'Household',
     'Sheltered Accommodation',
     'Hall of Residence',
@@ -16,8 +25,7 @@ VALID_ESTABLISHMENT_TYPES = {
     'Gypsy Roma Traveller',
     'Residential Boater'}
 
-
-VALID_TREATMENT_CODES = {
+TREATMENT_CODES = {
     'HH_LF2R1E', 'HH_LF2R2E', 'HH_LF2R3AE', 'HH_LF2R3BE', 'HH_LF3R1E', 'HH_LF3R2E', 'HH_LF3R3AE', 'HH_LF3R3BE',
     'HH_LFNR1E', 'HH_LFNR2E', 'HH_LFNR3AE', 'HH_LFNR3BE', 'HH_LF2R1W', 'HH_LF2R2W', 'HH_LF2R3AW', 'HH_LF2R3BW',
     'HH_LF3R1W', 'HH_LF3R2W', 'HH_LF3R3AW', 'HH_LF3R3BW', 'HH_LFNR1W', 'HH_LFNR2W', 'HH_LFNR3AW', 'HH_LFNR3BW',
@@ -25,314 +33,91 @@ VALID_TREATMENT_CODES = {
     'HH_QFNR1E', 'HH_QFNR2E', 'HH_QFNR3AE', 'HH_QF2R1W', 'HH_QF2R2W', 'HH_QF2R3AW', 'HH_QF3R1W', 'HH_QF3R2W',
     'HH_QF3R3AW', 'HH_QFNR1W', 'HH_QFNR2W', 'HH_QFNR3AW', 'HH_3QSFN'}
 
+COLUMN_VALIDATORS = {
+    'ARID': (Mandatory(), MaxLength(21), Unique()),
+    'ESTAB_ARID': (Mandatory(), MaxLength(21)),
+    'UPRN': (Mandatory(), MaxLength(12), IsNumeric()),
+    'ADDRESS_TYPE': (Mandatory(), InSet({"HH", "CE", "SPG"})),
+    'ESTAB_TYPE': (Mandatory(), InSet(ESTAB_TYPES)),
+    'ADDRESS_LEVEL': (Mandatory(), InSet({'E', 'U'})),
+    'ABP_CODE': (Mandatory(), MaxLength(6)),
+    'ORGANISATION_NAME': (MaxLength(60),),
+    'ADDRESS_LINE1': (Mandatory(), MaxLength(60)),
+    'ADDRESS_LINE2': (MaxLength(60),),
+    'ADDRESS_LINE3': (MaxLength(60),),
+    'TOWN_NAME': (Mandatory(), MaxLength(30)),
+    'POSTCODE': (Mandatory(), MaxLength(8)),
+    'LATITUDE': (Mandatory(), IsFloat(), MaxDecimalScale(7), MaxDecimalPrecision(9)),
+    'LONGITUDE': (Mandatory(), IsFloat(), MaxDecimalScale(7), MaxDecimalPrecision(10)),
+    'OA': (Mandatory(), MaxLength(9)),
+    'LSOA': (Mandatory(), MaxLength(9)),
+    'MSOA': (Mandatory(), MaxLength(9)),
+    'LAD': (Mandatory(), MaxLength(9)),
+    'REGION': (Mandatory(), MaxLength(9)),
+    'HTC_WILLINGNESS': (Mandatory(), MaxLength(9), InSet({'0', '1', '2', '3', '4', '5'})),
+    'HTC_DIGITAL': (Mandatory(), MaxLength(9), InSet({'0', '1', '2', '3', '4', '5'})),
+    'FIELDCOORDINATOR_ID': (MaxLength(7),),
+    'FIELDOFFICER_ID': (MaxLength(10),),
+    'TREATMENT_CODE': (Mandatory(), MaxLength(10), InSet(TREATMENT_CODES)),
+    'CE_EXPECTED_CAPACITY': (MaxLength(4), IsNumeric()),
+}
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(description='Load a sample file into response management.')
-    parser.add_argument('sample_file_path', help='path to the sample file', type=str)
-    return parser.parse_args()
+ValidationFailure = namedtuple('ValidationFailure', ('line_number', 'column', 'description'))
 
 
-args = parse_arguments()
-error_count = 0
+def validate_fieldnames(fieldnames):
+    valid_header = set(COLUMN_VALIDATORS.keys())
+    try:
+        SetEqual(valid_header)(fieldnames)
+    except ValidationError as err:
+        return ValidationFailure(1, None, str(err))
 
 
-def validate_header_row(fieldnames):
-    valid_header = {'ARID', 'ESTAB_ARID', 'UPRN', 'ADDRESS_TYPE', 'ESTAB_TYPE', 'ADDRESS_LEVEL', 'ABP_CODE',
-                    'ORGANISATION_NAME', 'ADDRESS_LINE1', 'ADDRESS_LINE2', 'ADDRESS_LINE3', 'TOWN_NAME', 'POSTCODE',
-                    'LATITUDE', 'LONGITUDE', 'OA', 'LSOA', 'MSOA', 'LAD', 'REGION', 'HTC_WILLINGNESS',
-                    'HTC_DIGITAL', 'FIELDCOORDINATOR_ID', 'FIELDOFFICER_ID', 'TREATMENT_CODE',
-                    'CE_EXPECTED_CAPACITY'}
-
-    if not set(fieldnames) == valid_header:
-        print(f'Header is invalid.')
-        exit(-1)
+def find_validation_failures(sample_file_reader) -> list:
+    failures = []
+    for line_number, row in enumerate(sample_file_reader, 2):
+        failures.extend(find_row_validation_failures(line_number, row))
+    return failures
 
 
-def validate_sample_file(sample_file_path):
+def find_row_validation_failures(line_number, row):
+    failures = []
+    for column, validators in COLUMN_VALIDATORS.items():
+        for validator in validators:
+            try:
+                validator(row[column])
+            except ValidationError as validation_failure:
+                failures.append(ValidationFailure(line_number, column, validation_failure))
+    return failures
+
+
+def validate_sample_file(sample_file_path) -> list:
     try:
         with open(sample_file_path, encoding="utf-8") as sample_file:
-            return load_sample(sample_file)
+            sample_file_reader = csv.DictReader(sample_file, delimiter=',')
+            header_failures = validate_fieldnames(sample_file_reader.fieldnames)
+            if header_failures:
+                return [header_failures]
+            return find_validation_failures(sample_file_reader)
     except UnicodeDecodeError as err:
-        print(f'Invalid file encoding, requires utf-8, error: {err}')
-        exit(-1)
-
-
-def load_sample(sample_file):
-    sample_file_reader = csv.DictReader(sample_file, delimiter=',')
-    validate_header_row(sample_file_reader.fieldnames)
-    for line_number, sample_row in enumerate(sample_file_reader, 2):
-        validate_arid(line_number, sample_row)
-        validate_estab_arid(line_number, sample_row)
-        validate_uprn(line_number, sample_row)
-        validate_address_type(line_number, sample_row)
-        validate_estab_type(line_number, sample_row)
-        validate_address_level(line_number, sample_row)
-        validate_abp_code(line_number, sample_row)
-        validate_org_name(line_number, sample_row)
-        validate_address_line(line_number, sample_row)
-        validate_town_name(line_number, sample_row)
-        validate_postcode(line_number, sample_row)
-        validate_longitude_latitude(line_number, sample_row, 'LATITUDE', max_scale=7, max_precision=10)
-        validate_longitude_latitude(line_number, sample_row, 'LONGITUDE', max_scale=7, max_precision=9)
-        validate_oa(line_number, sample_row)
-        validate_lsoa(line_number, sample_row)
-        validate_msoa(line_number, sample_row)
-        validate_lad(line_number, sample_row)
-        validate_region(line_number, sample_row)
-        validate_htc_willingness(line_number, sample_row)
-        validate_htc_digital(line_number, sample_row)
-        validate_fieldcordinator_id(line_number, sample_row)
-        validate_fieldofficer_id(line_number, sample_row)
-        validate_treatment_code(line_number, sample_row)
-        validate_ce_expected_capacity(line_number, sample_row)
-
-
-def validate_arid(line_number, sample_row):
-    global error_count
-    column = 'ARID'
-    maximum_length = 21
-    if _check_mandatory_value_exists(line_number, column, mandatory=True, sample_row=sample_row):
-        _check_length(column, sample_row[column], line_number, maximum_length)
-    if sample_row[column] in ARID:
-        print(f'Line {line_number}: {column}: {sample_row[column]} is duplicated in sample file.')
-        error_count += 1
-    else:
-        ARID.add(sample_row[column])
-
-
-def validate_estab_arid(line_number, sample_row):
-    column = 'ESTAB_ARID'
-    maximum_length = 21
-    if _check_mandatory_value_exists(line_number, column, mandatory=True, sample_row=sample_row):
-        _check_length(column, sample_row[column], line_number, maximum_length)
-
-
-def validate_uprn(line_number, sample_row):
-    global error_count
-    column = 'UPRN'
-    maximum_length = 12
-    if _check_mandatory_value_exists(line_number, column, mandatory=False, sample_row=sample_row):
-        _check_length(column, sample_row[column], line_number, maximum_length)
-        uprn = sample_row[column]
-        if not uprn.isnumeric() and uprn != '':
-            print(f'Line {line_number}: {column}: {sample_row[column]} is not a valid integer.')
-            error_count += 1
-
-
-def validate_address_type(line_number, sample_row):
-    global error_count
-    column = 'ADDRESS_TYPE'
-    if _check_mandatory_value_exists(line_number, column, mandatory=True, sample_row=sample_row):
-        address_type = sample_row[column]
-        if address_type not in {"HH", "CE", "SPG"}:
-            print(f'Line {line_number}: {column}: {sample_row[column]} is not valid.')
-            error_count += 1
-
-
-def validate_estab_type(line_number, sample_row):
-    column = 'ESTAB_TYPE'
-    if _check_mandatory_value_exists(line_number, column, mandatory=True, sample_row=sample_row):
-        _is_valid_estab_type(line_number, sample_row[column])
-
-
-def validate_address_level(line_number, sample_row):
-    global error_count
-    column = 'ADDRESS_LEVEL'
-    if _check_mandatory_value_exists(line_number, column, mandatory=True, sample_row=sample_row):
-        if sample_row[column] not in {"E", "U"}:
-            print(f'Line {line_number}: {column}: {sample_row[column]} is not valid.')
-            error_count += 1
-
-
-def validate_abp_code(line_number, sample_row):
-    column = 'ABP_CODE'
-    maximum_length = 6
-    if _check_mandatory_value_exists(line_number, column, mandatory=True, sample_row=sample_row):
-        _check_length(column, sample_row[column], line_number, maximum_length)
-
-
-def validate_org_name(line_number, sample_row):
-    column = 'ORGANISATION_NAME'
-    maximum_length = 60
-    if _check_mandatory_value_exists(line_number, column, mandatory=False, sample_row=sample_row):
-        _check_length(column, sample_row[column], line_number, maximum_length)
-
-
-def validate_address_line(line_number, sample_row):
-    maximum_length = 60
-    # only address line 1 is mandatory
-    column = 'ADDRESS_LINE1'
-    if _check_mandatory_value_exists(line_number, column, mandatory=True, sample_row=sample_row):
-        for column in ['ADDRESS_LINE1', 'ADDRESS_LINE2', 'ADDRESS_LINE3']:
-            address_line = sample_row[column]
-            _check_length(column, address_line, line_number, maximum_length)
-
-
-def validate_town_name(line_number, sample_row):
-    column = 'TOWN_NAME'
-    maximum_length = 30
-    if _check_mandatory_value_exists(line_number, column, mandatory=True, sample_row=sample_row):
-        _check_length(column, sample_row[column], line_number, maximum_length)
-
-
-def validate_postcode(line_number, sample_row):
-    column = 'POSTCODE'
-    maximum_length = 8
-    if _check_mandatory_value_exists(line_number, column, mandatory=True, sample_row=sample_row):
-        _check_length(column, sample_row[column], line_number, maximum_length)
-
-
-def validate_longitude_latitude(line_number, sample_row, column, max_scale, max_precision):
-    # Precision is the total number of digits and scale is the total number of digits after the decimal point
-    global error_count
-    numeric = True
-    if _check_mandatory_value_exists(line_number, column, mandatory=True, sample_row=sample_row):
-        num, decimal = sample_row[column].split(".")
-        if num[0] == '-':
-            num = num[1:]
-        precision = len(num) + len(decimal)
-        scale = len(decimal)
-        try:
-            float(sample_row[column])
-        except ValueError:
-            numeric = False
-        if precision > max_precision or scale > max_scale or not numeric:
-            print(f'Line {line_number}: {column}: {sample_row[column]} is not valid. '
-                  f'Value needs to be numeric with max precision of {max_precision} and max scale of {max_scale}')
-            error_count += 1
-
-
-def validate_oa(line_number, sample_row):
-    column = 'OA'
-    maximum_length = 9
-    if _check_mandatory_value_exists(line_number, column, mandatory=True, sample_row=sample_row):
-        _check_length(column, sample_row[column], line_number, maximum_length)
-
-
-def validate_lsoa(line_number, sample_row):
-    column = 'LSOA'
-    maximum_length = 9
-    if _check_mandatory_value_exists(line_number, column, mandatory=True, sample_row=sample_row):
-        _check_length(column, sample_row[column], line_number, maximum_length)
-
-
-def validate_msoa(line_number, sample_row):
-    column = 'MSOA'
-    maximum_length = 9
-    if _check_mandatory_value_exists(line_number, column, mandatory=True, sample_row=sample_row):
-        _check_length(column, sample_row[column], line_number, maximum_length)
-
-
-def validate_lad(line_number, sample_row):
-    column = 'LAD'
-    maximum_length = 9
-    if _check_mandatory_value_exists(line_number, column, mandatory=True, sample_row=sample_row):
-        _check_length(column, sample_row[column], line_number, maximum_length)
-
-
-def validate_region(line_number, sample_row):
-    column = 'REGION'
-    maximum_length = 9
-    if _check_mandatory_value_exists(line_number, column, mandatory=True, sample_row=sample_row):
-        _check_length(column, sample_row[column], line_number, maximum_length)
-
-
-def validate_htc_willingness(line_number, sample_row):
-    global error_count
-    column = 'HTC_WILLINGNESS'
-    if _check_mandatory_value_exists(line_number, column, mandatory=True, sample_row=sample_row):
-        htc_willingness = sample_row[column]
-        if htc_willingness.isnumeric and len(htc_willingness) == 1:
-            return
-        print(f'Line {line_number}: {column} {htc_willingness} is not valid.')
-        error_count += 1
-
-
-def validate_htc_digital(line_number, sample_row):
-    global error_count
-    column = 'HTC_DIGITAL'
-    if _check_mandatory_value_exists(line_number, column, mandatory=True, sample_row=sample_row):
-        htc_digital = sample_row[column]
-        if htc_digital.isnumeric and len(htc_digital) == 1:
-            return
-        print(f'Line {line_number}: {column} {htc_digital} is not valid.')
-        error_count += 1
-
-
-def validate_fieldcordinator_id(line_number, sample_row):
-    column = 'FIELDCOORDINATOR_ID'
-    maximum_length = 7
-    if _check_mandatory_value_exists(line_number, column, mandatory=False, sample_row=sample_row):
-        _check_length(column, sample_row[column], line_number, maximum_length)
-
-
-def validate_fieldofficer_id(line_number, sample_row):
-    column = 'FIELDOFFICER_ID'
-    maximum_length = 10
-    if _check_mandatory_value_exists(line_number, column, mandatory=False, sample_row=sample_row):
-        _check_length(column, sample_row[column], line_number, maximum_length)
-
-
-def validate_treatment_code(line_number, sample_row):
-    column = 'TREATMENT_CODE'
-    maximum_length = 10
-    mandatory = True
-    if _check_mandatory_value_exists(line_number, column, mandatory, sample_row):
-        _check_length(column, sample_row[column], line_number, maximum_length)
-        _is_valid_treatment_code(line_number, sample_row[column])
-
-
-def validate_ce_expected_capacity(line_number, sample_row):
-    global error_count
-    column = 'CE_EXPECTED_CAPACITY'
-    maximum_length = 4
-    if _check_mandatory_value_exists(line_number, column, mandatory=False, sample_row=sample_row):
-        value = sample_row[column]
-        _check_length(column, value, line_number, maximum_length)
-        if not value == "" and not value.isnumeric():
-            print(f'Line {line_number}: {column}: {value} is not a valid integer.')
-            error_count += 1
-
-
-def _check_mandatory_value_exists(line_number, column, mandatory, sample_row):
-    global error_count
-    # check value is not empty
-    value = sample_row[column]
-    if mandatory and not value:
-        print(f'Line {line_number}: {column} is empty.')
-        error_count += 1
-        return False
-    return True
-
-
-def _check_length(name, value, line_number, maximum_length):
-    global error_count
-    if len(value) > maximum_length:
-        print(f'Line {line_number}: {name}: {value} exceeds maximum length of {maximum_length}.')
-        error_count += 1
-
-
-def _is_valid_treatment_code(line_number, treatment_code):
-    global error_count
-    if treatment_code not in VALID_TREATMENT_CODES:
-        print(f'Line {line_number}: TREATMENT_CODE: {treatment_code} is invalid.')
-        error_count += 1
-
-
-def _is_valid_estab_type(line_number, estab_type):
-    global error_count
-    if estab_type not in VALID_ESTABLISHMENT_TYPES:
-        print(f'Line {line_number}: ESTAB_TYPE: {estab_type} is invalid.')
-        error_count += 1
+        return [ValidationFailure(None, None, f'Invalid file encoding, requires utf-8, error: {err}')]
 
 
 def main():
-    validate_sample_file(args.sample_file_path)
-    if error_count == 0:
-        print(f'Sample file is OK.')
-    else:
-        print(f'{error_count} error(s) found in sample file')
-        exit(-1)
+    args = parse_arguments()
+    failures = validate_sample_file(args.sample_file_path)
+    if failures:
+        print(f'{len(failures)} validation failure(s):')
+        for failure in failures:
+            failure_log = (f'line: {failure.line_number}, column: {failure.column}, description: {failure.description}'
+                           if failure.column else
+                           f'line: Header, description: {failure.description}'
+                           if failure.line_number else
+                           failure.description)
+            print(failure_log)
+        print(f'{args.sample_file_path} is not valid ❌')
+        exit(1)
+    print(f'Success! {args.sample_file_path} passed validation ✅')
 
 
 if __name__ == "__main__":
---
+++ validators.py
@@ -0,0 +1,108 @@
+from typing import Iterable
+
+
+class ValidationError(Exception):
+    pass
+
+
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class Validator:
+    def __call__(self, value) -> None:
+        return self.validate(value)
+
+    def validate(self, value) -> None:
+        pass
+
+
+class MaxLength(Validator):
+    def __init__(self, max_length):
+        self.max_length = max_length
+
+    def validate(self, value):
+        if len(value) > self.max_length:
+            raise ValidationError(f'Value has length {len(value)}, exceeds maximum length of {self.max_length}')
+
+
+class Unique(Validator):
+    def __init__(self):
+        self.previous_values = set()
+
+    def validate(self, value):
+        if value in self.previous_values:
+            raise ValidationError(f'Value "{value}" is not unique')
+        self.previous_values.add(value)
+
+
+class Mandatory(Validator, metaclass=Singleton):
+    def validate(self, value):
+        if not value:
+            raise ValidationError(f'Empty mandatory value')
+
+
+class IsNumeric(Validator, metaclass=Singleton):
+    def validate(self, value):
+        if value and not value.isnumeric():
+            raise ValidationError(f'Value "{value}" is non numeric')
+
+
+class IsFloat(Validator, metaclass=Singleton):
+    def validate(self, value) -> None:
+        try:
+            float(value)
+        except ValueError:
+            raise ValidationError(f'Value "{value}" is not a valid float')
+
+
+class MaxDecimalPrecision(Validator):
+    def __init__(self, max_decimal_precision):
+        self.max_decimal_precision = max_decimal_precision
+
+    def validate(self, value) -> None:
+        integer, decimal = value.split('.')
+        integer = integer.strip('-')
+        precision = len(integer) + len(decimal)
+        if precision > self.max_decimal_precision:
+            raise ValidationError(
+                f'Value {value} as a precision of {precision},'
+                f' exceeds max of {self.max_decimal_precision}')
+
+
+class MaxDecimalScale(Validator):
+    def __init__(self, max_decimal_scale):
+        self.max_decimal_scale = max_decimal_scale
+
+    def validate(self, value) -> None:
+        scale = len(value.split('.')[1])
+        if scale > self.max_decimal_scale:
+            raise ValidationError(
+                f'Value {value} as a scale of {scale},'
+                f' exceeds max of {self.max_decimal_scale}')
+
+
+class InSet(Validator):
+    def __init__(self, valid_value_set):
+        self.value_set = valid_value_set
+
+    def validate(self, value) -> None:
+        if value not in self.value_set:
+            raise ValidationError(f'Value "{value}" is not valid')
+
+
+class SetEqual(Validator):
+    def __init__(self, expected_set):
+        self.expected_set = expected_set
+
+    def validate(self, value: Iterable) -> None:
+        value_as_set = set(value)
+        if value_as_set != self.expected_set:
+            raise ValidationError((f"Values don't match expected set, "
+                                   f'missing values: {self.expected_set.difference(value_as_set)},'
+                                   f' unexpected values: {value_as_set.difference(self.expected_set)}'))
