ce0c86ac180dbdf46ab2744a4ef7874efec00171
LeoKHoward <leo-howard@hotmail.co.uk>
2020-10-05 08:46:21 +0100

    Renamed validator method

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -292,19 +292,19 @@ class TestValidators(TestCase):
         with pytest.raises(validators.Invalid):
             latitude_longitude_range_validator(360)
 
-    def test_alphanumeric_field_values_valid(self):
+    def test_alphanumeric_plus_hyphen_field_values_valid(self):
         # Given
-        alphanumeric_field_validator = validators.alphanumeric_field_values()
+        alphanumeric_plus_hyphen_field_validator = validators.alphanumeric_plus_hyphen_field_values()
 
         # When
-        alphanumeric_field_validator('TE-STT1-ES-01')
+        alphanumeric_plus_hyphen_field_validator('TE-STT1-ES-01')
 
         # Then no invalid exception is raised
 
-    def test_alphanumeric_field_values_invalid(self):
+    def test_alphanumeric_plus_hyphen_field_values_invalid(self):
         # Given
-        alphanumeric_field_validator = validators.alphanumeric_field_values()
+        alphanumeric_plus_hyphen_field_validator = validators.alphanumeric_plus_hyphen_field_values()
 
         # When, then raises
         with pytest.raises(validators.Invalid):
-            alphanumeric_field_validator('TE-STT1-ES-!!')
+            alphanumeric_plus_hyphen_field_validator('TE-STT1-ES-!!')
