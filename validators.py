1d8a809fc6b8c7ade8c076ec77b74ca75fc86d0d
Adam <35268982+AdamHawtin@users.noreply.github.com>
2020-10-23 12:10:30 +0100

    Enforce lat longs in the format integer, decimal point, integer (#48)

--- validators.py
+++ validators.py
@@ -49,7 +49,11 @@ def latitude_longitude(max_precision: int, max_scale: int):
         except ValueError:
             raise Invalid(f'Value "{value}" is not a valid float')
         try:
+            # This will fail validation correctly if given a value with none or multiple '.'s
+            # as this line raises ValueError if it cannot unpack into 2 variables.
             integer, decimal = value.split('.')
+            int(integer)
+            int(decimal)
         except ValueError:
             raise Invalid(f'Malformed decimal, Value = "{value}"')
         integer = integer.strip('-')
