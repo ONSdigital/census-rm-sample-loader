a86316b1bcf705077b41d2f161301380852b111a
Leo Howard <38526889+LeoKHoward@users.noreply.github.com>
2020-12-07 12:55:24 +0000

    CE/SPG Sample File Comparison Checks (#50)

---
+++ compare_ce_spg_sample_file_with_original.py
@@ -0,0 +1,63 @@
+import argparse
+import csv
+
+
+def compare_files(old_file_path, new_file_path):
+    old_file_by_uprn = {}
+
+    problems_found = []
+
+    with open(old_file_path) as old_file:
+
+        old_file_reader = csv.DictReader(old_file, delimiter=',')
+
+        for count, sample_row in enumerate(old_file_reader, 1):
+            old_file_by_uprn[f'{sample_row["UPRN"]}'] = sample_row
+
+    with open(new_file_path) as new_file:
+
+        new_file_reader = csv.DictReader(new_file, delimiter=',')
+
+        unique_uprns = set()
+
+        for count, sample_row in enumerate(new_file_reader, 1):
+            matching_sample_row = old_file_by_uprn.get(sample_row['UPRN'])
+            if not matching_sample_row:
+                problems_found.append(f'Could not find UPRN in original sample {sample_row["UPRN"]} on row {count + 1}')
+            else:
+                if sample_row["UPRN"] in unique_uprns:
+                    problems_found.append(f'Duplicate UPRN {sample_row["UPRN"]} on row {count + 1}')
+
+                unique_uprns.add(sample_row['UPRN'])
+
+                for row_key in list(sample_row.keys()):
+                    if sample_row[row_key] != matching_sample_row[row_key] \
+                            and row_key != 'FIELDCOORDINATOR_ID' and row_key != 'FIELDOFFICER_ID':
+                        problems_found.append(f'Found invalid data in column {row_key}, row {count + 1}:'
+                                              f' "{sample_row[row_key]}" '
+                                              f'but expected: "{matching_sample_row[row_key]}"')
+
+    return problems_found
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Validate new file against old one')
+    parser.add_argument('old_file', help='Old file to process', type=str)
+    parser.add_argument('new_file', help='New file to process', type=str)
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
+
+    problems_found = compare_files(old_file_path=args.old_file, new_file_path=args.new_file)
+    if problems_found:
+        print('\n'.join(problems_found))
+        print('This file has FAILED validation')
+        exit(1)
+    else:
+        print('This file has PASSED validation')
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_compare_ce_spg_sample_file_with_original.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+
+from compare_ce_spg_sample_file_with_original import compare_files
+
+RESOURCE_FILE_PATH = Path(__file__).parent.joinpath('resources/compare_ce_spg_files')
+
+
+def test_pass_validation_if_rows_removed_in_new_file():
+    old_sample_file = RESOURCE_FILE_PATH.joinpath('ce_spg_old.csv')
+
+    new_sample_file = RESOURCE_FILE_PATH.joinpath('ce_spg_new_removed_rows.csv')
+
+    problems_found = compare_files(old_sample_file, new_sample_file)
+
+    assert len(problems_found) == 0
+
+
+def test_fail_validation_if_duplicated_lines_in_new_file():
+    old_sample_file = RESOURCE_FILE_PATH.joinpath('ce_spg_old.csv')
+
+    new_sample_file = RESOURCE_FILE_PATH.joinpath('ce_spg_new_duplicated_lines.csv')
+
+    problems_found = compare_files(old_sample_file, new_sample_file)
+
+    expected_problems = ['Duplicate UPRN 12348874419 on row 6', 'Duplicate UPRN 12348874419 on row 7',
+                         'Duplicate UPRN 12348874419 on row 8']
+
+    assert problems_found == expected_problems
+
+
+def test_fail_validation_if_new_rows_in_new_file():
+    old_sample_file = RESOURCE_FILE_PATH.joinpath('ce_spg_old.csv')
+
+    new_sample_file = RESOURCE_FILE_PATH.joinpath('ce_spg_new_brand_new_uprns.csv')
+
+    problems_found = compare_files(old_sample_file, new_sample_file)
+
+    expected_problems = ['Could not find UPRN in original sample 12345 on row 5']
+
+    assert problems_found == expected_problems
+
+
+def test_fail_validation_if_columns_other_than_field_officer_and_field_coordinator_are_changed():
+    old_sample_file = RESOURCE_FILE_PATH.joinpath('ce_spg_old.csv')
+
+    new_sample_file = RESOURCE_FILE_PATH.joinpath('ce_spg_new_postcode_changed.csv')
+
+    problems_found = compare_files(old_sample_file, new_sample_file)
+
+    expected_problems = ['Found invalid data in column POSTCODE, row 3: "XXXXXX" but expected: "OO9 5DX"']
+
+    assert problems_found == expected_problems
+
+
+def test_pass_validation_if_field_officer_and_field_coordinator_are_changed():
+    old_sample_file = RESOURCE_FILE_PATH.joinpath('ce_spg_old.csv')
+
+    new_sample_file = RESOURCE_FILE_PATH.joinpath('ce_spg_new_field_columns_changed.csv')
+
+    problems_found = compare_files(old_sample_file, new_sample_file)
+
+    assert len(problems_found) == 0
